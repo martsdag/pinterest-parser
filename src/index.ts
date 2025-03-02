0a3b938fe5497c054b2f88699601e33525aae1ae
martsdag <clarte.lisse.31@gmail.com>
2025-03-02 12:51:37 +0400

    + image parser

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
 import puppeteer from 'puppeteer';
+import commandLineArgs from 'command-line-args';
+
+const optionDefinitions = [{name:'query', alias: 'q', type: String, multiple: true }];
+
+const options = commandLineArgs(optionDefinitions);
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  // const q = 'cats';
+
 
   try {
-    await page.goto(`https://ru.pinterest.com/search/pins/?q=${process.argv[2]}`, {'waitUntil':'networkidle0'});
+    await page.goto(`https://ru.pinterest.com/search/pins/?q=${options.query.join(" ")}`, {'waitUntil':'networkidle0'});
+    
+    const data = await page.evaluate(async () => {
+      window.scrollTo(0, document.body.scrollHeight);
 
-    const data = await page.evaluate(() => {
-      const resultSelector: NodeListOf<HTMLImageElement> = document.querySelectorAll('a[href^="/pin"] img');
+      const resultSelector: NodeListOf<HTMLImageElement> = document.querySelectorAll('a[href^="/pin"] img:not([src^="https://i.pinimg.com/videos"])');
       const resultSelectorArray = Array.from(resultSelector);
       return resultSelectorArray.map((img: HTMLImageElement) => img.src);
     });
 
-    console.log('Ссылки:', data);
+    console.log('Ссылки:', data, data.length);
   } catch (error) {
     console.error('Ошибка:', error);
   } finally {
     await browser.close();
   }
 })();
-
-console.log('Аргументы командной строки:', process.argv);
